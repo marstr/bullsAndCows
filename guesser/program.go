aacf2817757203d1d4e9f656b902e41767f9c61f
Martin Strobel <marstr@microsoft.com>
2017-08-15 14:35:35 -0700

    Guesser seems to be working.

--- guesser/program.go
+++ guesser/program.go
@@ -11,15 +11,46 @@ func main() {
 
 	for i := 0; i < 10000; i++ {
 		parsed, err := bc.ParseNumber(fmt.Sprint(i))
-		if err != nil {
+		if err == nil {
+			fmt.Printf("Adding Possiblity: %q\n", parsed)
 			remaining = append(remaining, parsed)
 		}
 	}
 
+	fmt.Printf("Starting off, there are %d valid numbers you may have chosen.\n", len(remaining))
+
+	guesses := 0
+
 	for {
+		if len(remaining) == 0 {
+			fmt.Println("I give up!")
+			return
+		}
+
 		guess := remaining[0]
 
 		fmt.Println("My guess: ", guess)
 
+		var bulls, cows uint8
+		fmt.Print("Cows: ")
+		fmt.Scan(&cows)
+		fmt.Print("Bulls: ")
+		fmt.Scan(&bulls)
+
+		if len(guess) == int(bulls) {
+			fmt.Printf("It took me %d tries to guess your number.\n", guesses)
+			return
+		}
+
+		var next []bc.Number
+		for _, entry := range remaining {
+			if entry.Bulls(guess) == bulls && entry.Cows(guess) == cows {
+				next = append(next, entry)
+			}
+		}
+
+		fmt.Printf("I've narrowed it down to %d options:\n", len(next))
+		remaining = next
+		guesses++
 	}
 }
